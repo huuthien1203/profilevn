c5cf06abc518688069cee530c8ecbcb09704d2ef
huuthien1203 <88137574+huuthien1203@users.noreply.github.com>
2021-08-12 08:43:05 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$("#a1").click(function() {
+    $('#text').slideToggle('slow');
+});
+$("#a2").click(function() {
+    $('#text1').slideToggle('slow');
+});
+$("#a3").click(function() {
+    $('#text2').slideToggle('slow');
+});
+$("#a4").click(function() {
+    $('#text3').slideToggle('slow');
+});
